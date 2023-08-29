a2fafd3f6003cd830ad41771e7bc7ae0a007b17d
NecRaul <necraul@kuroneko.dev>
2023-08-29 12:12:51 +0400

    Check for file size Pomf doesn't accept files bigger than 1GB

--- lain_upload/upload.py
+++ lain_upload/upload.py
@@ -1,9 +1,17 @@
 import requests
+import os
+
+GIGABYTE = 1000000000
 
 API_ENDPOINT = "https://pomf.lain.la/upload.php"
 
 def upload_file(file_path):
     with open(file_path, 'rb') as file:
+        try:
+            file_size = os.path.getsize(file_path)
+            if (file_size >= GIGABYTE):
+                return None
+            else:
                 files = {'files[]': (file_path, file)}
         
                 response = requests.post(API_ENDPOINT, files=files)
@@ -12,3 +20,5 @@ def upload_file(file_path):
                     return response.json()
                 else:
                     return None
+        except:
+            return None
