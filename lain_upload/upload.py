4ed853aa8b12c2ac10b6fb17483e5d1357fc2067
NecRaul <necraul@kuroneko.dev>
2023-08-29 10:23:18 +0400

    Upload request

---
+++ lain_upload/upload.py
@@ -0,0 +1,13 @@
+import requests
+
+API_ENDPOINT = "https://pomf.lain.la/upload.php"
+
+def upload_file(file_path):
+    with open(file_path, 'rb') as file:
+                files = {'files[]': (file_path, file)}
+        
+                response = requests.post(API_ENDPOINT, files=files)
+                
+                return response.json()
+
+print(upload_file("pomf.png")) # test case
