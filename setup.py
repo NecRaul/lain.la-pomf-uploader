c3b34bb14d6a5b9b1878c063dd4028d071a167aa
NecRaul <necraul2001@gmail.com>
2023-08-29 12:07:05 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0"
+DESCRIPTION = "pomf.lain.la uploader."
+LONG_DESCRIPTION = "Uploading files to pomf.lain.la using a python interface."
+AUTHOR = "NecRaul"
+
+setup(
+    name="lain_upload",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    packages=find_packages(),
+    install_requires=["requests", "setuptools"],
+    keywords=["python", "uploader", "pomf", "lain", "lain.la", "pomf.lain.la"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP"
+    ],
+    py_modules=["upload"],
+    entry_points={
+        "console_scripts": [
+            "lain-upload = lain_upload.__init__:main",
+        ],
+    },
+)
