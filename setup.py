3ab75bb91f3292f3759e3ef0cb76b7665fe89bbe
NecRaul <necraul@kuroneko.dev>
2024-01-22 10:57:10 +0400

    Changed version to 1.7

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.6"
+VERSION = "1.7"
 DESCRIPTION = "pomf.lain.la uploader."
 with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
