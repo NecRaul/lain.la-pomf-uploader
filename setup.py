da34bbdec837cb1e4338c79b1d52b9ac0721b797
NecRaul <necraul@kuroneko.dev>
2025-06-24 10:47:33 +0400

    Update setup.py
    Add author_email
    Add Issues to project_urls
    Remove LICENSE from classifiers
    Remove install_requires
    Fix entry_points

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ DESCRIPTION = "pomf.lain.la uploader."
 with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
 AUTHOR = "NecRaul"
+AUTHOR_EMAIL = "necraul@kuroneko.dev"
 
 setup(
     name="lain_upload",
@@ -13,19 +14,19 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
     packages=find_packages(),
-    install_requires=["requests", "pyperclip"],
     keywords=["python", "uploader", "pomf", "lain", "lain.la", "pomf.lain.la"],
     url="https://github.com/NecRaul/lain.la-pomf-uploader",
     project_urls={
         "Documentation": "https://github.com/NecRaul/lain.la-pomf-uploader#readme",
         "Source": "https://github.com/NecRaul/lain.la-pomf-uploader",
+        "Issues": "https://github.com/NecRaul/lain.la-pomf-uploader/issues",
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
@@ -34,7 +35,7 @@ setup(
     py_modules=["upload"],
     entry_points={
         "console_scripts": [
-            "lain-upload = lain_upload.main:main",
+            "lain-upload = lain_upload:main",
         ],
     },
 )
